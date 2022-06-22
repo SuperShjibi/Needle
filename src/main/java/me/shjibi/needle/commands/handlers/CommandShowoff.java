b146a30fcfb00c139d1c8cca80aa2eed1498ea87
SuperShjibi <shjibi@outlook.com>
2022-06-22 19:01:45 +0800

    Updated custom join message

---
+++ src/main/java/me/shjibi/needle/commands/handlers/CommandShowoff.java
@@ -0,0 +1,21 @@
+package me.shjibi.needle.commands.handlers;
+
+import me.shjibi.needle.Main;
+import me.shjibi.needle.commands.base.PlayerCommandHandler;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public final class CommandShowoff extends PlayerCommandHandler {
+
+    public CommandShowoff() {
+        super(Main.getInstance(), "showoff", 0, null);
+    }
+
+    @Override
+    protected void execute(Player sender, Command command, String label, String[] args) {
+        ItemStack item = sender.getInventory().getItemInMainHand();
+
+    }
+
+}
